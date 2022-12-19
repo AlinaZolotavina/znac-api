7c429c7a407af68802e30c5dae51a5b41e3f8667
Alina Zolotavina <abekmetova@yandex.ru>
2022-12-19 20:43:07 +0400

    fix: cors errors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cors = require('cors');
-const corsOptions = require('./utils/corsOptions');
+// const corsOptions = require('./utils/corsOptions');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -28,7 +28,21 @@ mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 });
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.options(('*', cors()));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://znac.org');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS',
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(requestLogger);
 
